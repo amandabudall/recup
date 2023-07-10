c49bec4807d4f851b8953790a0756c14959b6049
amandabudall <amanda_b_lima@estudante.sesisenai.org.br>
2023-07-10 10:55:01 -0300

    add

--- conecta.php
+++ conecta.php
@@ -4,7 +4,7 @@
     
     // CONEXÃO COM A BASE DE DADOS: verifique sempre o dbname
     try{
-        $pdo = new PDO("mysql:dbname=lista_tel;host=localhost;charset=utf8","root","");
+        $pdo = new PDO("mysql:dbname=recupelista;host=localhost;charset=utf8","root","");
     }
     catch(PDOException $erro)
     {
